2d82d6051d71ce85d233609cb67f927ade021518
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-08-13 12:14:46 +0530

    Fixed Bugs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let number = 5;
     print!("Multipy by 2 is {}", multipy_by_2(number))
 }
-fn multipy_by_2(intial_number) -> i32 {
+fn multipy_by_2(intial_number : i32) -> i32 {
     let new_number = &intial_number *2;
     return new_number;
 }
