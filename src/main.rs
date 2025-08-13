a2d50431e59fc43d23218a0ed41bc6dfd54468dd
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-08-13 12:24:58 +0530

    Done COmpleted

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let number = 5;
-    print!("Multipy by 2 is {}", multipy_by_2(number))
+    println!("Multipy by 2 is {}", multipy_by_2(number));
+    print!("Inital number was {}", number);
 }
 fn multipy_by_2(intial_number : i32) -> i32 {
     let new_number = &intial_number *2;
